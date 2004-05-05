6c4201955ad4a47da0aa7c9d0836b3e1078018b6
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-05-05 16:10:41 +0000

    adding vignettes to windows pulldown
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@8508 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,11 @@
     if( !require("Biobase")) stop("can only load GOstats with Biobase")
     if( !require("graph")) stop("can only load GOstats with graph")
     if( !require("GO")) stop("can only load GOstats with GO")
+
+    if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
+        && .Platform$GUI ==  "Rgui"){
+        addPDF2Vig("GOstats")
+    }
+
+
 }
