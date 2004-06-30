9d63b8246b1c1e00cdd6f22a4d81a07db3fe99f5
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-06-30 17:47:20 +0000

    check the package versions on loading
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@9312 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,71 @@
 .First.lib <- function(libname, pkgname, where) {
-    if( !require("Biobase")) stop("can only load GOstats with Biobase")
-    if( !require("graph")) stop("can only load GOstats with graph")
-    if( !require("GO")) stop("can only load GOstats with GO")
+    ## require's version specificity doesn't allow for patterns.
+    ## These will simulate it, in a hackish manner
+    installed <- installed.packages()
+
+    ## Make sure the metadata is at least 1.6.x
+    ## Metadata is only 'Suggests' tho, so ok if they
+    ## don't have it.
+    cur <- match("hgu95av2", installed[,"Package"])
+    if (!is.na(cur)) {
+        if (compareVersion(installed[cur,"Version"], "1.6.0") < 0)
+            stop("Your version of hgu95av2 must be at least 1.6.x")
+        else
+            require("hgu95av2")
+    }
+
+    ## The other packages are dependencies, must have them.  Make
+    ## sure the BioC packages are the devel versions (as of 30/06/04)
+
+    cur <- match("GO", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.6.0")
+                      < 0))
+        stop("You must have package GO version >= 1.6.0")
+    else
+        require("GO")
+
+    cur <- match("graph", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.1")
+                      < 0))
+        stop("You must have package graph version >= 1.4.1")
+    else
+        require("graph")
+
+    cur <- match("annotate", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.0")
+                      < 0))
+        stop("You must have package annotate version >= 1.4.0")
+    else
+        require("annotate")
+
+    cur <- match("Biobase", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.15")
+                      < 0))
+        stop("You must have package Biobase version >= 1.4.15")
+    else
+        require("Biobase")
+
+    cur <- match("RBGL", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.2.2")
+                      < 0))
+        stop("You must have package RBGL version >= 1.2.2")
+    else
+        require("RBGL")
+
+    cur <- match("genefilter", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.0")
+                      < 0))
+        stop("You must have package genefilter version >= 1.4.0")
+    else
+        require("genefilter")
+
+    cur <- match("multtest", installed[,"Package"])
+    if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.1")
+                      < 0))
+        stop("You must have package multtest version >= 1.4.1")
+    else
+        require("multtest")
+
 
     if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
         && .Platform$GUI ==  "Rgui"){
