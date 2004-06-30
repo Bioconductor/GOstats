4c845cbfc8d458a43fe76af9d62ddeb42182b17e
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-06-30 18:06:45 +0000

    change stop() to warning()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@9313 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -9,7 +9,7 @@
     cur <- match("hgu95av2", installed[,"Package"])
     if (!is.na(cur)) {
         if (compareVersion(installed[cur,"Version"], "1.6.0") < 0)
-            stop("Your version of hgu95av2 must be at least 1.6.x")
+            warning("Your version of hgu95av2 must be at least 1.6.x")
         else
             require("hgu95av2")
     }
@@ -20,49 +20,49 @@
     cur <- match("GO", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.6.0")
                       < 0))
-        stop("You must have package GO version >= 1.6.0")
+        warning("You must have package GO version >= 1.6.0")
     else
         require("GO")
 
     cur <- match("graph", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.1")
                       < 0))
-        stop("You must have package graph version >= 1.4.1")
+        warning("You must have package graph version >= 1.4.1")
     else
         require("graph")
 
     cur <- match("annotate", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.0")
                       < 0))
-        stop("You must have package annotate version >= 1.4.0")
+        warning("You must have package annotate version >= 1.4.0")
     else
         require("annotate")
 
     cur <- match("Biobase", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.15")
                       < 0))
-        stop("You must have package Biobase version >= 1.4.15")
+        warning("You must have package Biobase version >= 1.4.15")
     else
         require("Biobase")
 
     cur <- match("RBGL", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.2.2")
                       < 0))
-        stop("You must have package RBGL version >= 1.2.2")
+        warning("You must have package RBGL version >= 1.2.2")
     else
         require("RBGL")
 
     cur <- match("genefilter", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.0")
                       < 0))
-        stop("You must have package genefilter version >= 1.4.0")
+        warning("You must have package genefilter version >= 1.4.0")
     else
         require("genefilter")
 
     cur <- match("multtest", installed[,"Package"])
     if ((!is.na(cur))&&(compareVersion(installed[cur,"Version"], "1.4.1")
                       < 0))
-        stop("You must have package multtest version >= 1.4.1")
+        warning("You must have package multtest version >= 1.4.1")
     else
         require("multtest")
 
