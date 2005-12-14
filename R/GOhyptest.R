03d45c54bfa9f24adc3afdcd5e924d0018d5665e
Ting-Yuan Liu <tliu@fhcrc.org>
2005-12-14 22:44:39 +0000

    Add a new argument "universe" to the GOHyperG function Add a new function GOKEGGHyperG which is an extension of GOHyperG
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@15392 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GOhyptest.R
+++ R/GOhyptest.R
@@ -1,6 +1,6 @@
 ##now we just sample K from the LLs and generate the GO graph
 
-GOHyperG <- function(x, lib="hgu95av2", what="MF") {
+GOHyperG <- function(x, lib="hgu95av2", what="MF", universe=NULL) {
 
    ##a helper function
     getDataEnv <- function(name, lib) {
@@ -20,12 +20,28 @@ GOHyperG <- function(x, lib="hgu95av2", what="MF") {
     ##except for YEAST where it maps to common names
 
     if( lib == "YEAST")
-        cLLs <- unique(as.list(YEASTCOMMON2SYSTEMATIC), keep.names=FALSE)
+      cLLs <- unique(unlist(as.list(YEASTGO2ALLPROBES)))
     else
-        cLLs <- unlist(as.list(getDataEnv("LOCUSID", lib)))
+      cLLs <- unique(unlist(as.list(getDataEnv("LOCUSID", lib))))
 
+    if(!is.null(universe)){
+        if(any(duplicated(universe))){
+            stop("universe must be unique")
+        }
+        goodUniverse <- universe %in% cLLs
+        if(!all(goodUniverse)){
+            ##FIXME:  any better statement for this?
+            warning(paste("There are ", sum(!goodUniverse)), " out of ", length(goodUniverse), " elements in the universe which are not in the ", lib)
+        }
+        cLLs <- universe[goodUniverse]
+    }
+    
     ##which ones do we have - don't worry about duplicates just yet
-    ourLLs <- cLLs[match(x, cLLs)]
+    goodX <- x %in% cLLs
+    if(!all(goodX)){
+        warning(paste("There are ", sum(!goodX), " out of ", length(goodX), " elememts in x which are not in the population"))
+    }
+    ourLLs <- x[x %in% cLLs]
 
     ##map to the unique LLs for each GO term
     goV <- as.list(getDataEnv("GO2ALLPROBES", lib))
@@ -40,31 +56,58 @@ GOHyperG <- function(x, lib="hgu95av2", what="MF") {
     #aa <- sapply(a2GOsz, length)
     #a2GO <- unique(unlist(sapply(a2GO, names)))
 
-    if( lib == "YEAST" )
-        whWeHave <- sapply(goV, function(y) {
-            if( is.na(y) || length(y) == 0 )
-                return(FALSE)
-            any(x %in% y)} )
-    else
-        whWeHave <- sapply(goV, function(y) {
-            if( is.na(y) || length(y) == 0 )
-                return(FALSE)
-            lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
-            ifnotfound=NA)))
-            any(x %in% lls) })
-
-    goV <- goV[whWeHave]
+    ## use the subset of the whole GO stree
+    if(!is.null(universe)){
+        if( lib == "YEAST" ) {
+            goV <- lapply(goV,
+                          function(y){
+                              if(all(unlist(y) %in% cLLs)) return(y)
+                          })
+            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
+        }else{
+            goV <- lapply(goV,
+                          function(y){
+                              lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
+                                ifnotfound=NA)))
+                              if(all(lls %in% cLLs)) return(y)
+                          })
+            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
+        }
+    }else{
+        ## remove some non-interested samples in order to increase the speed
+        if( lib == "YEAST" ){
+            whWeHave <- sapply(goV,
+                               function(y) {
+                                   if( is.na(y) || length(y) == 0 )
+                                     return(FALSE)
+                                   any(ourLLs %in% y)
+                               }
+                               )
+        }else{
+            whWeHave <- sapply(goV,
+                               function(y) {
+                                   if( is.na(y) || length(y) == 0 )
+                                     return(FALSE)
+                                   lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
+                                     ifnotfound=NA)))
+                                   any(ourLLs %in% lls)
+                               }
+                               )
+            
+            goV <- goV[whWeHave]
+        }
+    }
 
     ##fix the all node problem
-
     dropAll = match("all", names(goV), 0)
     if( dropAll > 0 )
         goV = goV[-dropAll]
 
-
     ##Determine which of the GO terms are in the category we are
     ##working on
-    goCat <- unlist(getGOOntology(names(goV)))
+    ## fixed by Ting: getGOOntology will return a vector, not list
+    ## goCat <- unlist(getGOOntology(names(goV)))
+    goCat <- getGOOntology(names(goV))
     goodGO <- goCat == what
     ##not every thing in goV has a category
     mm <- match(names(goCat), names(goV))
@@ -76,9 +119,9 @@ GOHyperG <- function(x, lib="hgu95av2", what="MF") {
     if( lib == "YEAST")
         goVcts = goV
     else
-        goVcts = sapply(goV, function(x) {
-            if(length(x) == 0 || is.na(x) ) return(NA)
-            lls <- unique(unlist(mget(x, getDataEnv("LOCUSID", lib))))
+        goVcts = sapply(goV, function(y) {
+            if(length(y) == 0 || is.na(y) ) return(NA)
+            lls <- unique(unlist(mget(y, getDataEnv("LOCUSID", lib))))
             lls<-lls[!is.na(lls)]
             lls
         })
@@ -93,8 +136,159 @@ GOHyperG <- function(x, lib="hgu95av2", what="MF") {
 
     goCounts <- sapply(goVcts, length)
 
-    ourLLs <- unique(ourLLs[!is.na(ourLLs)])
-    ours <-ourLLs[!duplicated(ourLLs)]
+    ours <- unique(ourLLs[!is.na(ourLLs)])
+    
+    ##need to get the number of interesting genes - these
+    ##are the ones supplied that map to some GO term
+    whGood <- ours[ours %in% cLLs]
+
+
+    nInt = length(whGood)
+    if( nInt == 0 )
+       warning("no interesting genes found")
+
+    useCts <- sapply(goVcts, function(y) sum(whGood %in% y))
+
+    ##need the -1 because we are asking for evidence as extreme or
+    ##more extreme than what we say - and we are using the upper tail
+    pvs <- phyper(useCts-1, nInt, nLL-nInt, goCounts, lower.tail=FALSE)
+
+    ord <- order(pvs)
+    return(list(pvalues=pvs[ord], goCounts=goCounts[ord], chip=lib,
+                go2Affy=goV, intCounts=useCts[ord], numLL=nLL,
+                numInt = nInt, intLLs=x))
+}
+
+    ##
+
+GOKEGGHyperG <- function (x, lib = "hgu95av2", what = "MF", universe=NULL)
+{
+
+    getDataEnv <- function(name, lib) {
+        get(paste(lib, name, sep = ""), mode = "environment")
+    }
+
+
+
+    require(lib, character.only = TRUE) || stop("need data package", lib)
+
+    if (any(duplicated(x)))
+      stop("input IDs must be unique")
+    
+    ################  MODIFIED #################
+    match.arg(what, c("MF", "BP", "CC", "KEGG"))
+    ############################################
+
+
+    if( lib == "YEAST")
+      cLLs <- unique(unlist(as.list(YEASTGO2ALLPROBES)))
+    else
+      cLLs <- unique(unlist(as.list(getDataEnv("LOCUSID", lib))))
+
+    if(!is.null(universe)){
+        if(any(duplicated(universe))){
+            stop("universe must be unique")
+        }
+        goodUniverse <- universe %in% cLLs
+        if(!all(goodUniverse)){
+            ##FIXME:  any better statement for this?
+            warning(paste("There are ", sum(!goodUniverse)), " out of ", length(goodUniverse), " elements in the universe which are not in the ", lib)
+        }
+        cLLs <- universe[goodUniverse]
+    }
+
+    goodX <- x %in% cLLs
+    if(!all(goodX)){
+        warning(paste("There are ", sum(!goodX), " out of ", length(goodX), " elememts in x which are not in the population"))
+    }
+    ourLLs <- x[x %in% cLLs]
+
+    
+    ################## MODIFIED ###########################
+    if (what=="KEGG") {
+        goV <- as.list(getDataEnv("PATH2PROBE", lib))
+    } else {
+        goV <- as.list(getDataEnv("GO2ALLPROBES", lib))
+    }
+    ######################################################
+
+    ## use the subset of the whole GO stree
+    if(!is.null(universe)){
+        if( lib == "YEAST" ) {
+            goV <- lapply(goV,
+                          function(y){
+                              if(all(unlist(y) %in% cLLs)) return(y)
+                          })
+            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
+        }else{
+            goV <- lapply(goV,
+                          function(y){
+                              lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
+                                ifnotfound=NA)))
+                              if(all(lls %in% cLLs)) return(y)
+                          })
+            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
+        }
+    }else{
+        ## remove some non-interested samples in order to increase the speed
+        if( lib == "YEAST" ){
+            whWeHave <- sapply(goV,
+                               function(y) {
+                                   if( is.na(y) || length(y) == 0 )
+                                     return(FALSE)
+                                   any(ourLLs %in% y)
+                               }
+                               )
+        }else{
+            whWeHave <- sapply(goV,
+                               function(y) {
+                                   if( is.na(y) || length(y) == 0 )
+                                     return(FALSE)
+                                   lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
+                                     ifnotfound=NA)))
+                                   any(ourLLs %in% lls)
+                               }
+                               )
+            
+            goV <- goV[whWeHave]
+        }
+    }
+
+    ##################  MODIFIED #########################
+    if (what!="KEGG") {
+        dropAll = match("all", names(goV), 0)
+        if( dropAll > 0 )
+          goV = goV[-dropAll]
+
+        goCat <- unlist(getGOOntology(names(goV)))
+        goodGO <- goCat == what
+        mm <- match(names(goCat), names(goV))
+        mm <- mm[goodGO]
+        goV <- goV[mm]
+    }
+    ######################################################
+
+    if( lib == "YEAST")
+      goVcts = goV
+    else
+      goVcts = sapply(goV, function(x) {
+          if(length(x) == 0 || is.na(x) ) return(NA)
+          lls <- unique(unlist(mget(x, getDataEnv("LOCUSID", lib))))
+          lls<-lls[!is.na(lls)]
+          lls
+      })
+
+    bad <- sapply(goVcts, function(x) (length(x) == 1 && is.na(x)))
+    goVcts = goVcts[!bad]
+    goV = goV[!bad] ##we are going to output this...
+
+    ##find out how many map which is m+n in phyper-speak
+    cLLs <- unique(unlist(goVcts))
+    nLL <- length(cLLs)
+
+    goCounts <- sapply(goVcts, length)
+
+    ours <- unique(ourLLs[!is.na(ourLLs)])
     ##need to get the number of interesting genes - these
     ##are the ones supplied that map to some GO term
     whGood <- ours[ours %in% cLLs]
@@ -111,9 +305,19 @@ GOHyperG <- function(x, lib="hgu95av2", what="MF") {
     pvs <- phyper(useCts-1, nInt, nLL-nInt, goCounts, lower.tail=FALSE)
 
     ord <- order(pvs)
-    return(list(pvalues=pvs[ord], goCounts=goCounts[ord], chip=lib,
-                go2Affy=goV, intCounts=useCts[ord], numLL=nLL,
-                numInt = nInt, intLLs=x))
-}
 
-    ##
+        ################################  MODIFIED
+##################################################
+    if (what!="KEGG") {
+        return(list(pvalues = pvs[ord], goCounts = goCounts[ord], chip = lib, go2Affy =
+goV,
+        intCounts = useCts[ord], numLL = nLL, numInt = nInt, intLLs = x))
+    } else {
+        return(list(pvalues = pvs[ord], keggCounts = goCounts[ord], chip = lib, kegg2Affy =
+goV,
+        intCounts = useCts[ord], numLL = nLL, numInt = nInt, intLLs = x))
+    }
+    ################################  MODIFIED
+##################################################
+
+}
