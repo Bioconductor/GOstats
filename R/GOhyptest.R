a11d46e16c90978746e0328a9136fbd4eca3b166
Seth Falcon <sfalcon@fhcrc.org>
2006-09-05 23:02:37 +0000

    Move GOHyperG back to GOstats
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@19629 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GOhyptest.R
+++ R/GOhyptest.R
@@ -1,165 +1,35 @@
-##now we just sample K from the LLs and generate the GO graph
+resultToGOHyperG <- function(res, origIds) {
+    go2Affy <- mget(names(pvalues(res)),
+                    Category:::getDataEnv("GO2ALLPROBES", annotation(res)))
+    list(pvalues=pvalues(res),
+         goCounts=universeCounts(res),
+         intCounts=geneCounts(res),
+         numLL=universeMappedCount(res),
+         numInt=geneMappedCount(res),
+         chip=annotation(res),
+         intLLs=origIds,
+         go2Affy=go2Affy)
+}
 
-## GOHyperG moved to the Category package
+
+GOHyperG <- function(x, lib, what="MF", universe=NULL)
+{
+    if (missing(universe) || is.null(universe))
+      universe <- character(0)
+    params <- new("GeneGoHyperGeoTestParams",
+                  geneIds=x,
+                  universeGeneIds=universe,
+                  annotation=lib,
+                  ontology=what)
+    if (missing(lib) || !is.character(lib))
+      stop("argument ", sQuote("lib"), " must be character")
+
+    res <- geneCategoryHyperGeoTest(params)
+    resultToGOHyperG(res, origIds=x)
+}
+
 
 GOKEGGHyperG <- function (x, lib = "hgu95av2", what = "MF", universe=NULL)
 {
     .Defunct("geneKeggHyperGeoTest", "Category")
-
-    getDataEnv <- function(name, lib) {
-        get(paste(lib, name, sep = ""), mode = "environment")
-    }
-
-
-
-    require(lib, character.only = TRUE) || stop("need data package", lib)
-
-    if (any(duplicated(x)))
-      stop("input IDs must be unique")
-    
-    ################  MODIFIED #################
-    match.arg(what, c("MF", "BP", "CC", "KEGG"))
-    ############################################
-
-
-    if( lib == "YEAST")
-      cLLs <- unique(unlist(as.list(YEASTGO2ALLPROBES)))
-    else
-      cLLs <- unique(unlist(as.list(getDataEnv("LOCUSID", lib))))
-
-    if(!is.null(universe)){
-        if(any(duplicated(universe))){
-            stop("universe must be unique")
-        }
-        goodUniverse <- universe %in% cLLs
-        if(!all(goodUniverse)){
-            ##FIXME:  any better statement for this?
-            warning(paste("There are ", sum(!goodUniverse)), " out of ", length(goodUniverse), " elements in the universe which are not in the ", lib)
-        }
-        cLLs <- universe[goodUniverse]
-    }
-
-    goodX <- x %in% cLLs
-    if(!all(goodX)){
-        warning(paste("There are ", sum(!goodX), " out of ", length(goodX), " elememts in x which are not in the population"))
-    }
-    ourLLs <- x[x %in% cLLs]
-
-    
-    ################## MODIFIED ###########################
-    if (what=="KEGG") {
-        goV <- as.list(getDataEnv("PATH2PROBE", lib))
-    } else {
-        goV <- as.list(getDataEnv("GO2ALLPROBES", lib))
-    }
-    ######################################################
-
-    ## use the subset of the whole GO stree
-    if(!is.null(universe)){
-        if( lib == "YEAST" ) {
-            goV <- lapply(goV,
-                          function(y){
-                              if(all(unlist(y) %in% cLLs)) return(y)
-                          })
-            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
-        }else{
-            goV <- lapply(goV,
-                          function(y){
-                              lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
-                                ifnotfound=NA)))
-                              if(all(lls %in% cLLs)) return(y)
-                          })
-            goV <- goV[which(sapply(goV, function(y) !is.null(y) ))]
-        }
-    }else{
-        ## remove some non-interested samples in order to increase the speed
-        if( lib == "YEAST" ){
-            whWeHave <- sapply(goV,
-                               function(y) {
-                                   if( is.na(y) || length(y) == 0 )
-                                     return(FALSE)
-                                   any(ourLLs %in% y)
-                               }
-                               )
-        }else{
-            whWeHave <- sapply(goV,
-                               function(y) {
-                                   if( is.na(y) || length(y) == 0 )
-                                     return(FALSE)
-                                   lls = unique(unlist(mget(y, getDataEnv("LOCUSID", lib),
-                                     ifnotfound=NA)))
-                                   any(ourLLs %in% lls)
-                               }
-                               )
-            
-            goV <- goV[whWeHave]
-        }
-    }
-
-    ##################  MODIFIED #########################
-    if (what!="KEGG") {
-        dropAll = match("all", names(goV), 0)
-        if( dropAll > 0 )
-          goV = goV[-dropAll]
-
-        goCat <- unlist(getGOOntology(names(goV)))
-        goodGO <- goCat == what
-        mm <- match(names(goCat), names(goV))
-        mm <- mm[goodGO]
-        goV <- goV[mm]
-    }
-    ######################################################
-
-    if( lib == "YEAST")
-      goVcts = goV
-    else
-      goVcts = sapply(goV, function(x) {
-          if(length(x) == 0 || is.na(x) ) return(NA)
-          lls <- unique(unlist(mget(x, getDataEnv("LOCUSID", lib))))
-          lls<-lls[!is.na(lls)]
-          lls
-      })
-
-    bad <- sapply(goVcts, function(x) (length(x) == 1 && is.na(x)))
-    goVcts = goVcts[!bad]
-    goV = goV[!bad] ##we are going to output this...
-
-    ##find out how many map which is m+n in phyper-speak
-    cLLs <- unique(unlist(goVcts))
-    nLL <- length(cLLs)
-
-    goCounts <- sapply(goVcts, length)
-
-    ours <- unique(ourLLs[!is.na(ourLLs)])
-    ##need to get the number of interesting genes - these
-    ##are the ones supplied that map to some GO term
-    whGood <- ours[ours %in% cLLs]
-
-
-    nInt = length(whGood)
-    if( nInt == 0 )
-       warning("no interesting genes found")
-
-    useCts <- sapply(goVcts, function(x) sum(whGood %in% x))
-
-    ##need the -1 because we are asking for evidence as extreme or
-    ##more extreme than what we say - and we are using the upper tail
-    pvs <- phyper(useCts-1, nInt, nLL-nInt, goCounts, lower.tail=FALSE)
-
-    ord <- order(pvs)
-
-        ################################  MODIFIED
-##################################################
-    if (what!="KEGG") {
-        return(list(pvalues = pvs[ord], goCounts = goCounts[ord], chip = lib, go2Affy =
-goV,
-        intCounts = useCts[ord], numLL = nLL, numInt = nInt, intLLs = x))
-    } else {
-        return(list(pvalues = pvs[ord], keggCounts = goCounts[ord], chip = lib, kegg2Affy =
-goV,
-        intCounts = useCts[ord], numLL = nLL, numInt = nInt, intLLs = x))
-    }
-    ################################  MODIFIED
-##################################################
-
 }
--- inst/UnitTests/goHyperGeoTest_test.R
+++ inst/UnitTests/goHyperGeoTest_test.R
@@ -2,13 +2,11 @@ makeSimpleGeneGoHyperGeoTestParams <- function() {
     set.seed(344)
     probeIds <- ls(hgu95av2LOCUSID)
     randProbeIds <- sample(probeIds, 500)
-##     entrezUniverse <- unlist(mget(randProbeIds, hgu95av2LOCUSID,
-##                                   ifnotfound=NA))
     ## This is "wrong", should unlist, but the code
-    ## should catch/correct it.  The right way is above.
+    ## should catch/correct it.  The right way is to
+    ## unlist the mget result.
     entrezUniverse <- mget(randProbeIds, hgu95av2LOCUSID,
                            ifnotfound=NA)
-
     entrezUniverse <- entrezUniverse[!is.na(entrezUniverse)]
     selectedEntrezIds <- sample(entrezUniverse, 30)
     params <- new("GeneGoHyperGeoTestParams",
@@ -40,7 +38,9 @@ test_goHyperGeoTest_regression2 <- function() {
     p@conditional <- FALSE
     res <- goHyperGeoTest(p)
     checkEquals(18, sum(pvalues(res) < res@pvalue.cutoff))
-    
+
+    ## Verify result is same using geneCategoryHyperGeoTest
+    ## from Category
     res2 <- geneCategoryHyperGeoTest(p)
     checkEquals(pvalues(res), pvalues(res2))
     checkEquals(geneCounts(res), geneCounts(res2))
@@ -49,4 +49,35 @@ test_goHyperGeoTest_regression2 <- function() {
     checkEquals(geneMappedCount(res), geneMappedCount(res2))
     checkEquals(annotation(res), annotation(res2))
     checkEquals(testName(res), testName(res2))
+
+    ## Regression tests
+        pvals <- round(c(0.01596240, 0.01825930, 0.02194761), 3)
+    names(pvals) <- c("GO:0043170", "GO:0044265", "GO:0009057") 
+    checkEquals(pvals, round(pvalues(res)[1:3], 3))
+    
+    gcounts <- c(13, 4, 4)
+    names(gcounts) <- c("GO:0043170", "GO:0044265", "GO:0009057")
+    checkEquals(gcounts, geneCounts(res)[1:3])
+
+    ucounts <- c(134, 19, 20)
+    names(ucounts) <- c("GO:0043170", "GO:0044265", "GO:0009057")
+    checkEquals(ucounts, universeCounts(res)[1:3])
+    
+    checkEquals(381, universeMappedCount(res))
+    checkEquals(22, geneMappedCount(res))
+    checkEquals("hgu95av2", annotation(res))
+    checkEquals(c("GO", "BP"), testName(res))
+
+    ## Test conversion to legacy result
+
+    ## Check conversion to GOHyperG output format
+    ## We use a serialized result and test only the first
+    ## 10 entries (otherwise the saved result is too large).
+    ghg <- GOstats:::resultToGOHyperG(res, p@geneIds)
+    load(system.file("UnitTests/ghgans10.rda",
+                     package="GOstats"))
+    ghg10_expected <- ghgans10 ## from load
+    ghg10 <- lapply(ghg, function(x) if (length(x) > 11) x[1:10] else x)
+    checkEquals(ghg10_expected, ghg10)
+
 }
