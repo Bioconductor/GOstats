bf25f2c5a340bcf929b7829262dfe199e6ad2ec8
Seth Falcon <sfalcon@fhcrc.org>
2007-02-12 21:07:22 +0000

    Use generics defined in Category instead of redeclaring
    Recent code additions to Category define generics that were previously
    only defined in GOstats.  The double declaration causes problems on
    some systems (with binary packages?).
    
    /
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@22595 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -2,8 +2,3 @@ setGeneric("getGoGraph", function(p, goIds) standardGeneric("getGoGraph"))
 
 setGeneric("goDag", function(r) standardGeneric("goDag"))
 
-setGeneric("geneIdUniverse", function(r) standardGeneric("geneIdUniverse"))
-
-setGeneric("condGeneIdUniverse",
-           function(r) standardGeneric("condGeneIdUniverse"))
-
