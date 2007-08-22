346e21bb23f122a812279823d91186f032b93f05
Seth Falcon <sfalcon@fhcrc.org>
2007-08-22 19:47:37 +0000

    Backward compat fix for makeGOGraph, simLL for DB-based ann pkg use
    We introduce new arguments at the end of the signature, mapfun and
    chip, which allow for DB-based ann pkg compatibility without breaking
    existing code.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@26596 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GOgraph.R
+++ R/GOgraph.R
@@ -1,13 +1,57 @@
  combGOGraph = function(g1, g2) .Defunct("join", package = "GOstats")
 
-##given a set of LOCUSLINK IDs obtain the GO graph that has all
-##GO ids that those genes are annotated at, and
-makeGOGraph <- function (x, Ontology, eg2go.map, removeRoot=TRUE)
+.get_eg_to_go_fun <- function(mapfun, chip) {
+    if (!is.null(chip) && is.character(chip)) {
+        if (!is.null(mapfun) && is.function(mapfun))
+          warning("ignoring 'chip' argument in favor of 'mapfun'")
+        else {
+            ## create mapfun from chip
+            mapfun <- tryCatch({
+                eg2go <- make_eg_to_go_map(chip)
+                function(x) mget(x, eg2go, ifnotfound=NA)
+            }, error=function(e) {
+                ## if only we had classed exceptions!  and we can't
+                ## reliable grep the condition message since it may be
+                ## localized.
+                msg <- paste(conditionMessage(e),
+                             "\nDB-based version of ", chip, " not found.",
+                             "\nReverting to use of environment-based GO")
+                warning(msg, call.=FALSE)
+                NULL
+            })
+        }
+    }
+
+    if (!is.function(mapfun)) {
+        ## create mapfun from env-based GO
+        haveGO.env <- suppressWarnings(require("GO",
+                                               character.only=TRUE,
+                                               quietly=TRUE,
+                                               warn.conflicts=FALSE))
+        if (!haveGO.env) {
+            msg <- strwrap(paste("This function requires the environment",
+                                 "based GO package if neither 'mapfun' nor",
+                                 "'chip' are specified.  Note that this",
+                                 "is a different package from GO.db"))
+            stop(paste("GO package not found\n",
+                       paste(msg, collapse="\n")))
+        }
+        mapfun <- function(x)
+          mget(x, GOENTREZID2GO, ifnotfound=NA)
+    }
+    mapfun
+}
+
+## Given a set of Entrez IDs obtain the GO graph that has all
+## GO ids that those genes are annotated at, and
+makeGOGraph <- function (x, Ontology="MF", removeRoot=TRUE,
+                         mapfun=NULL, chip=NULL)
 {
+    eg2gofun <- .get_eg_to_go_fun(mapfun, chip)
     match.arg(Ontology, c("MF", "BP", "CC"))
     wh <- paste(Ontology, "PARENTS", sep = "")
     dataenv = GOenv(wh)
-    newNodes <- mget(x, envir = eg2go.map, ifnotfound=NA)
+    newNodes <- eg2gofun(x)
     if( length(newNodes) == 1)
        bd = is.na(newNodes[[1]])
     else
@@ -135,11 +179,12 @@ oneGOGraph <- function(x, dataenv) {
     max(plens)
 }
 
-simLL = function(ll1, ll2, Ontology, eg2go.map,
-                  measure="LP", dropCodes=NULL) {
+simLL = function(ll1, ll2, Ontology="MF", measure="LP", dropCodes=NULL,
+                 mapfun=NULL, chip=NULL) {
+    eg2gofun <- .get_eg_to_go_fun(mapfun, chip)
      ## helper function to get the right GOIDs
      .getWHEC = function(llid, wh, eCodes) {
-         x = eg2go.map[[llid]]
+         x = eg2gofun(llid)[[1L]]
          if( !is.null(eCodes) )
            x = dropECode(x, eCodes)
          unique(unlist(getOntology(x, wh)))
