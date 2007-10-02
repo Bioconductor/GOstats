b91edbda7e65e8bde24287ea0a742c4c8c284987
James MacDonald <jmacdon@med.umich.edu>
2007-10-02 18:05:55 +0000

    Made hyperGtable and hyperG2Affy defunct (replaced by summary and probeSetSummary, respectively). Added an example to probeSetSummary.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@27702 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hyperGtable.R
+++ R/hyperGtable.R
@@ -15,58 +15,69 @@
 ##  Moved hyperGtable and hyperG2Affy to GOstats 2-23-06
 ##
 ##  Added universe argument 3-22-06
+##
+## Made hyperGtable and hyperG3Affy defunct 10-2-07
 ###########################################
 
 hyperGtable <- function(probids, lib, type="MF", pvalue=0.05,
                         min.count=10, save = FALSE, output = TRUE,
                         filename = NULL, universe = NULL){
-    .Deprecated("summary")
-    message("use summary(result) where result is the output of hyperGTest")
-  require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
-  lls <- getLL(probids, lib)
-  lls <- unique(lls)
-  lls <- lls[!is.na(lls)]
-  tmp <- GOHyperG(lls, lib, type, universe)
-  index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
-  if(sum(index) == 0)
-    stop(paste("There are no significant GO terms using a p-value of ",pvalue,
-               " and a minimum count of ",  min.count,".\n", sep = ""), call. = FALSE)
-  wh <- mget(names(tmp$pvalues[index]), GOenv("TERM"))
-  tmp.terms <- sapply(wh, Term)
-  out <- data.frame(names(tmp$pvalues[index]), tmp.terms, round(tmp$pvalues[index], 3), 
-                       tmp$intCounts[index], tmp$goCounts[index])
-  names(out) <- c("GO Term","Description","p-values",
-                     "Number significant","Number on chip")
-  if(output){
-    if(!is.null(filename)){
-      write.table(out, paste(filename, ".txt", sep=""), sep="\t", quote=FALSE, row.names=FALSE)
-    }else{
-      stop("You need to give a filename in order to output this table!\n")
-    }
-  }
-  if(save)
-    out
+    .Defunct("summary", "Category", paste("\n\nThe hyperGtable function is defunct as of BioC 2.1\n",
+                                          "please use the summary method for HyperGResult objects.\n",
+                                          "See the GOstats vignette 'Hypergeometric Tests Using GOstats'\n",
+                                          "for more information.\n\n"))
 }
 
-hyperG2Affy <- function(probids, lib, type="MF", pvalue=0.05,
-                        min.count=10, universe = NULL){
-    .Deprecated("probeSetSummary")
-  require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
-  lls <- getLL(probids, lib)
-  lls <- unique(lls)
-  lls <- lls[!is.na(lls)]
-  tmp <- GOHyperG(lls, lib, type, universe)
-  index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
-  if(sum(index) == 0)
-    stop(paste("There are no significant GO terms using a p-value of ",pvalue,
-               " and a minimum count of ",  min.count,".\n", sep = ""), call. = FALSE)
-  wh <- mget(names(tmp$pvalues[index]), GOenv("TERM"))
-  tmp.terms <- sapply(wh, Term)
-  index2 <- match(names(tmp.terms), names(tmp$go2Affy))
-  out <- lapply(tmp$go2Affy[index2], function(x) x[x %in% probids])
-  names(out) <- names(tmp.terms)
-  out
-}
+##     .Deprecated("summary")
+##     message("use summary(result) where result is the output of hyperGTest")
+##   require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
+##   lls <- getLL(probids, lib)
+##   lls <- unique(lls)
+##   lls <- lls[!is.na(lls)]
+##   tmp <- GOHyperG(lls, lib, type, universe)
+##   index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
+##   if(sum(index) == 0)
+##     stop(paste("There are no significant GO terms using a p-value of ",pvalue,
+##                " and a minimum count of ",  min.count,".\n", sep = ""), call. = FALSE)
+##   wh <- mget(names(tmp$pvalues[index]), GOenv("TERM"))
+##   tmp.terms <- sapply(wh, Term)
+##   out <- data.frame(names(tmp$pvalues[index]), tmp.terms, round(tmp$pvalues[index], 3), 
+##                        tmp$intCounts[index], tmp$goCounts[index])
+##   names(out) <- c("GO Term","Description","p-values",
+##                      "Number significant","Number on chip")
+##   if(output){
+##     if(!is.null(filename)){
+##       write.table(out, paste(filename, ".txt", sep=""), sep="\t", quote=FALSE, row.names=FALSE)
+##     }else{
+##       stop("You need to give a filename in order to output this table!\n")
+##     }
+##   }
+##   if(save)
+##     out
+## }
+
+ hyperG2Affy <- function(probids, lib, type="MF", pvalue=0.05,
+                         min.count=10, universe = NULL){
+     .Defunct("probeSetSummary", "GOstats", paste("\n\nThe hyperG2Affy function is defunct as of BioC 2.1.\n",
+                                                  "Please use probeSetSummary instead.\n\n"))
+ }
+##     .Deprecated("probeSetSummary")
+##   require(lib, quietly = TRUE, character.only = TRUE) || stop(paste("The ", lib, " package is required"))
+##   lls <- getLL(probids, lib)
+##   lls <- unique(lls)
+##   lls <- lls[!is.na(lls)]
+##   tmp <- GOHyperG(lls, lib, type, universe)
+##   index <- tmp$pvalues < pvalue & tmp$goCounts > min.count
+##   if(sum(index) == 0)
+##     stop(paste("There are no significant GO terms using a p-value of ",pvalue,
+##                " and a minimum count of ",  min.count,".\n", sep = ""), call. = FALSE)
+##   wh <- mget(names(tmp$pvalues[index]), GOenv("TERM"))
+##   tmp.terms <- sapply(wh, Term)
+##   index2 <- match(names(tmp.terms), names(tmp$go2Affy))
+##   out <- lapply(tmp$go2Affy[index2], function(x) x[x %in% probids])
+##   names(out) <- names(tmp.terms)
+##   out
+## }
 
 
 probeSetSummary <- function(result, pvalue, categorySize, sigProbesets) {
