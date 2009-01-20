2b6ceb401c3140e38b61e26c76c1129043d9cd8a
Patrick Aboyoun <unknown>
2009-01-20 22:05:29 +0000

    Remove unneded .onLoad function.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@36843 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-    require("methods", quietly=TRUE)
-}
-
-
 .onAttach <- function(libname, pkgname) {
     if (.Platform$OS.type == "windows" && require("Biobase")
         && interactive() && .Platform$GUI == "Rgui") {
