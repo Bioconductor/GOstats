0d36c1034ba7a10d2625db57234ff40edeebfc2f
Marc Carlson <mcarlson@fhcrc.org>
2009-09-10 00:08:24 +0000

    Minor changes to GOstats to enable use with GSEABase
    Minor changes here to enable the use of GSEABase.  
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOstats@41601 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hyperGTest-methods.R
+++ R/hyperGTest-methods.R
@@ -1,76 +1,86 @@
-setMethod("hyperGTest",
-          signature(p="GOHyperGParams"), 
-          function(p) {
-              p <- makeValidParams(p)
-              p@universeGeneIds <- universeBuilder(p)
-              ## preserve names on geneIds
-              p@geneIds <- p@geneIds[p@geneIds %in% p@universeGeneIds]
-              ##FIXME: since the unique values in this list are the
-              ## the universe, it might be nice to have that somewhere
-              cat2Entrez <- categoryToEntrezBuilder(p)
-              ## build the GO graph for the relevant GO nodes
-              goIds <- names(cat2Entrez)
-              goDag <- getGoToChildGraph(p, goIds)
-              nodeDataDefaults(goDag, "pvalue") <- 1
-              nodeDataDefaults(goDag, "geneIds") <- numeric(0)
-              nodeDataDefaults(goDag, "condGeneIds") <- numeric(0)
-              nodeDataDefaults(goDag, "oddsRatio") <- numeric(0)
-              nodeDataDefaults(goDag, "expCount") <- numeric(0)
-              ## store the Entrez Gene IDs as attrs on the GO DAG.
-              nodeData(goDag, n=names(cat2Entrez),
-                       attr="geneIds") <- cat2Entrez
-              ## now iterate leaves first doing tests and conditioning
-              ## on all significant children.
-              ## FIXME: consider replacing with RBGL tsort?
-              needsProc <- goDag
-              complete <- character(0)
-              SIGNIF <- p@pvalueCutoff
-              while (length(nodes(needsProc))) {
-                  numKids <- sapply(edges(needsProc), length)
-                  noKids <- names(numKids[numKids == 0])
-                  curCat2Entrez <- cat2Entrez[noKids]
-                  if (p@conditional) {
-                      curCatKids <- edges(goDag)[names(curCat2Entrez)]
-                      curCatKids <- removeLengthZero(curCatKids)
-                      if (length(curCatKids)) { ## sanity check
-                          ## they should be all complete
-                          stopifnot(all(unlist(curCatKids) %in% complete))
-                      }
-                      curCat2Entrez <- removeSigKidGenes(curCatKids, goDag,
-                                                         curCat2Entrez,
-                                                         SIGNIF, cat2Entrez)
-                      ## Store the conditioned cat => entrez map
-                      nodeData(goDag, n=names(curCat2Entrez),
-                               attr="condGeneIds") <- curCat2Entrez
-                  }
-                  stats <- .doHyperGTest(p, curCat2Entrez, cat2Entrez,
-                                         p@geneIds)
+setMethod("hyperGTest", signature(p="GOHyperGParams"),
+          function(p) .hyperGTestInternal(p) )
 
-                  ## store the pvals, mark these nodes as complete,
-                  ## then compute the next set of nodes to do.  
-                  noKids <- names(curCat2Entrez)
-                  ## drop names on pvals to avoid weird names upon unlisting
-                  nodeData(goDag, n=noKids,
-                           attr="pvalue") <- as.numeric(stats$p)
-                  nodeData(goDag, n=noKids,
-                           attr="oddsRatio") <- as.numeric(stats$odds)
-                  nodeData(goDag, n=noKids,
-                           attr="expCount") <- as.numeric(stats$expected)
-                  complete <- c(complete, noKids)
-                  hasKids <- names(numKids[numKids > 0])
-                  needsProc <- subGraph(hasKids, needsProc)
-              } ## end while
-              pvals <- unlist(nodeData(goDag, attr="pvalue"))
-              new("GOHyperGResult",
-                  goDag=goDag,
-                  annotation=p@annotation,
-                  geneIds=p@geneIds,
-                  testName=categoryName(p),
-                  testDirection=p@testDirection,
-                  pvalueCutoff=p@pvalueCutoff,
-                  pvalue.order=order(pvals),
-                  conditional=p@conditional)
-          })
+.hyperGTestInternal <- function(p) {
+  p <- makeValidParams(p)
+  p@universeGeneIds <- universeBuilder(p)
+  ## preserve names on geneIds
+  p@geneIds <- p@geneIds[p@geneIds %in% p@universeGeneIds]
+  ##FIXME: since the unique values in this list are the
+  ## the universe, it might be nice to have that somewhere
+  cat2Entrez <- categoryToEntrezBuilder(p)
+  ## build the GO graph for the relevant GO nodes
+  goIds <- names(cat2Entrez)
+  goDag <- getGoToChildGraph(p, goIds)
+  nodeDataDefaults(goDag, "pvalue") <- 1
+  nodeDataDefaults(goDag, "geneIds") <- numeric(0)
+  nodeDataDefaults(goDag, "condGeneIds") <- numeric(0)
+  nodeDataDefaults(goDag, "oddsRatio") <- numeric(0)
+  nodeDataDefaults(goDag, "expCount") <- numeric(0)
+  ## store the Entrez Gene IDs as attrs on the GO DAG.
+  nodeData(goDag, n=names(cat2Entrez),
+           attr="geneIds") <- cat2Entrez
+  ## now iterate leaves first doing tests and conditioning
+  ## on all significant children.
+  ## FIXME: consider replacing with RBGL tsort?
+  needsProc <- goDag
+  complete <- character(0)
+  SIGNIF <- p@pvalueCutoff
+  while (length(nodes(needsProc))) {
+    numKids <- sapply(edges(needsProc), length)
+    noKids <- names(numKids[numKids == 0])
+    curCat2Entrez <- cat2Entrez[noKids]
+    if (p@conditional) {
+      curCatKids <- edges(goDag)[names(curCat2Entrez)]
+      curCatKids <- removeLengthZero(curCatKids)
+      if (length(curCatKids)) { ## sanity check
+        ## they should be all complete
+        stopifnot(all(unlist(curCatKids) %in% complete))
+      }
+      curCat2Entrez <- removeSigKidGenes(curCatKids, goDag,
+                                         curCat2Entrez,
+                                         SIGNIF, cat2Entrez)
+      ## Store the conditioned cat => entrez map
+      nodeData(goDag, n=names(curCat2Entrez),
+               attr="condGeneIds") <- curCat2Entrez
+    }
+    stats <- .doHyperGTest(p, curCat2Entrez, cat2Entrez,
+                           p@geneIds)
+    
+    ## store the pvals, mark these nodes as complete,
+    ## then compute the next set of nodes to do.  
+    noKids <- names(curCat2Entrez)
+    ## drop names on pvals to avoid weird names upon unlisting
+    nodeData(goDag, n=noKids,
+             attr="pvalue") <- as.numeric(stats$p)
+    nodeData(goDag, n=noKids,
+             attr="oddsRatio") <- as.numeric(stats$odds)
+    nodeData(goDag, n=noKids,
+             attr="expCount") <- as.numeric(stats$expected)
+    complete <- c(complete, noKids)
+    hasKids <- names(numKids[numKids > 0])
+    needsProc <- subGraph(hasKids, needsProc)
+  } ## end while
+  pvals <- unlist(nodeData(goDag, attr="pvalue"))
+  ##modded for future generic GenSet baseed objects...
+  if(class(p@datPkg)!="GeneSetCollectionDatPkg"){
+    annotation = annotation(p)
+  }else if(class(p@datPkg)=="GeneSetCollectionDatPkg"){
+    annotation = "Based on a GeneSetCollection Object"
+  }
+  new("GOHyperGResult",
+      goDag=goDag,
+      annotation=annotation,
+      geneIds=p@geneIds,
+      testName=categoryName(p),
+      testDirection=p@testDirection,
+      pvalueCutoff=p@pvalueCutoff,
+      pvalue.order=order(pvals),
+      conditional=p@conditional)
+}
+
+
+
 
 
 removeLengthZero <- function(x) {
